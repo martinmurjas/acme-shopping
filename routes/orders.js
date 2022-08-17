ce23e036111e1c215fac6ddbc42653de74d0965e
melanasia <melanie.ali54@gmail.com>
2022-08-17 00:10:11 +0000

    'order routes added'

--- routes/orders.js
+++ routes/orders.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express.Router();
 const { isLoggedIn } = require("./middleware");
+const { Order } = require("../db");
 
 module.exports = app;
 
@@ -24,6 +25,29 @@ app.get("/cart", isLoggedIn, async (req, res, next) => {
   try {
     res.send(await req.user.getCart());
   } catch (ex) {
+    next(ex);
+  }
+});
+
+app.get('/orders', async(req, res, next)=> {
+  try {
+    res.send(await Order.findAll());
+  }
+  catch(ex){
+    next(ex);
+  }
+});
+
+app.get('/orders/:id', async(req, res, next)=> {
+  try {
+    const id = req.params.id;
+    if (isNaN(id)) {
+        res.sendStatus(400)
+        return;
+    }
+    res.send(await Order.findOne({ where: { id } })); 
+  }
+  catch(ex){
     next(ex);
   }
 });
